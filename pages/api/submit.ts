ecd2909609f12206ab8a0a912bff2e7f2517daf1
mdlm-b <96142602+mdlm-b@users.noreply.github.com>
2022-07-15 11:37:53 -0400

    fix: Email format to lower case

--- pages/api/submit.ts
+++ pages/api/submit.ts
@@ -20,7 +20,7 @@ export default async function handler(
 
   try {
     await axios.post(url, JSON.stringify({
-      email,
+      email: email.toLowerCase(),
     }));
   } catch (error) {
     return res.status(500).json({ success: false });
